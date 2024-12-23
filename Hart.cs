eaa86015a57072196fcc31f9a810d8d6eaef06b6
jylack <jyl011202@gmail.com>
2024-12-23 18:49:23 +0900

    []

--- Hart.cs
+++ Hart.cs
@@ -11,28 +11,31 @@ namespace NecroDancer
         Point _point;
         int _len;
         Queue<Bit> bits;
-        string _image = "[     ]"; 
+        string _image = "[     ]";
+        int size;
 
- 
-        public Hart(Point point , int difficulty)
+
+        public Hart(Point point, int difficulty)
         {
-            
+
             bits = new Queue<Bit>();
-            
-            int size = _image.Length;
+
 
             _point = point;
+            size = _image.Length;
 
-            size = size / 2;
+            size = size / 2 + 1;
 
             //_point.PosX -= size;
 
             _len = difficulty;
         }
 
-        public void AddBit(Bit bit)
+        public void AddBit(int speed)
         {
-            bits.Enqueue(bit);
+
+            bits.Enqueue(new Bit(new Point(0, 0), speed, true)); //왼쪽
+            bits.Enqueue(new Bit(new Point(Console.WindowWidth, 0), speed,false ));//오른쪽 추가
         }
 
         public void RemoveBits()
@@ -48,23 +51,44 @@ namespace NecroDancer
                 bit.Move();
             }
         }
+
+        public Bit GetBit()
+        {
+            return bits.Peek();
+        }
+
         public bool IsBits()
         {
             return bits.Count > 0;
         }
-        public bool IsHit()
+        public bool IsNonCheckHit()
         {
             //left right 순서대로 비트 넣을거니까 0 2 4 같은 짝수는 전부다 왼쪽 비트 이다.
             //고로 맨앞에는 무조건 왼쪽 비트가 들어온다.
             Point point = bits.Peek().Point;
-            if (point.PosX >= _point.PosX +_len )
+
+            //
+            if (point.PosX >= _point.PosX + size)
             {
                 return true;
             }
-            
+            return false;
+        }
+
+        public bool IsCheckHit()
+        {
+            //left right 순서대로 비트 넣을거니까 0 2 4 같은 짝수는 전부다 왼쪽 비트 이다.
+            //고로 맨앞에는 무조건 왼쪽 비트가 들어온다.
+            Point point = bits.Peek().Point;
+            if (point.PosX >= _point.PosX &&
+                point.PosX <= _point.PosX + _len)
+            {
+                return true;
+            }
+
             //왼쪽 비트가 속도만큼 더해진 위치가 하트 위치와 같다면 
             //이러는 이유는 조금 빠른것 정도는 허용해주기 위해서
-            
+
             //비트의 위치가 하트보다 크거나 같지 않으면 어차피 안와서 false
             return false;
         }
@@ -74,14 +98,11 @@ namespace NecroDancer
             Console.SetCursorPosition(_point.PosX, _point.PosY);
             Console.WriteLine(_image);
 
-            foreach (var bit in bits) {
+            foreach (var bit in bits)
+            {
                 Console.SetCursorPosition(bit.Point.PosX, bit.Point.PosY);
                 Console.Write(bit.Image);
             }
-
-         
-
-
         }
 
     }
